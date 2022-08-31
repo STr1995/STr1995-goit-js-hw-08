f0a66c3e6b1c215f7ef322963a0b1de1b066b5ff
Serhii <99601512+STr1995@users.noreply.github.com>
2022-08-31 22:28:26 +0300

    Adds a library simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
+const galleryList = document.querySelector(".gallery");
+
+const galleryCard = galleryItems
+  .map(({ preview, original, description}) => `
+<a class="gallery__item" href="${original}">
+  <img
+  class="gallery__image"
+  src="${preview}"
+  alt="${description}" 
+  title = "${description}"/>
+</a>`
+  )
+  .join("");
+
+galleryList.innerHTML = galleryCard;
+
+new SimpleLightbox(".gallery a", { captionDelay: 250 });
 
 console.log(galleryItems);
